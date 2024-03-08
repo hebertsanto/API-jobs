3d1e99bb11b165b6269202bd8d02565e93108251
hebertsanto <hebertsantos0704@gmail.com>
2024-03-08 15:08:17 -0300

    refactor : Removing unnecessary validation and adjusting imports in the company exclusion controller

--- controllers/company/delete_company_controller.go
+++ controllers/company/delete_company_controller.go
@@ -5,30 +5,16 @@ import (
 	"strconv"
 	"vagas/infra/errors"
 	repository "vagas/infra/repository/company"
-	"vagas/models"
 	"vagas/pkg/logger"
 	services "vagas/services/company"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator"
 )
 
 func DeleteCompany(c *gin.Context) {
 
 	id := c.Param("id")
 
-	company := models.Company{}
-	if err := c.ShouldBindJSON(&company); err != nil {
-		logger.Log.Infof("Payload received in invalid. Payload: %+v\n", company)
-		errors.HandlerError(c, "BAD_REQUEST", err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	validate := validator.New()
-	if err := validate.Struct(company); err != nil {
-		logger.Log.Infof("error validating company data: %+v\n", company)
-		errors.HandlerError(c, "BAD_REQUEST", err.Error(), http.StatusBadRequest)
-	}
 	companyRepo := repository.NewCompanyRepository()
 
 	companyService := services.CompanyService{Repo: companyRepo}
