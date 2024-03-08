388420c882c3565e9a655d09dc884817a2f31c2d
hebertsanto <hebertsantos0704@gmail.com>
2024-03-08 15:09:33 -0300

    refactor : Remove User Profile Validation in Profile Get Controller

--- controllers/profile/get-profile_controller.go
+++ controllers/profile/get-profile_controller.go
@@ -3,32 +3,15 @@ package controllers
 import (
 	"vagas/infra/errors"
 	repository "vagas/infra/repository/profile"
-	"vagas/models"
 	"vagas/pkg/logger"
 	services "vagas/services/profile"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator"
 )
 
 func GetProfile(c *gin.Context) {
 
 	id := c.Param("id")
-	profile := models.UserProfile{}
-
-	if err := c.ShouldBindJSON(&profile); err != nil {
-		errors.HandlerError(c, "BAD_REQUEST", err.Error(), 400)
-		return
-	}
-
-	validate := validator.New()
-
-	if err := validate.Struct(profile); err != nil {
-		logger.Log.Infof("error validating user data: %+v\n", profile)
-		errors.HandlerError(c, "BAD_REQUEST", err.Error(), 400)
-		return
-	}
-
 	profileRepo := repository.CreateProfileRepository()
 
 	profileService := services.ProfileService{Repo: profileRepo}
