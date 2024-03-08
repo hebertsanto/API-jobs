64d3d3bb724e5464e3e1ac555f36a80c426ef6e8
hebertsanto <hebertsantos0704@gmail.com>
2024-03-08 14:33:48 -0300

    refactor : Removed user DTO files

--- dto/user_dto.go
+++
@@ -1,8 +0,0 @@
-package dtos
-
-type UserDto struct {
-	Name     string `json:"name" validate:"required"`
-	Password string `json:"password" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Cpf      string `json:"cpf" validate:"required"`
-}
